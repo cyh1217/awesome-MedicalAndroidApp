d5f31e26ef77094e676191212bc315b6ae464842
cyh1217 <630094635@qq.com>
2018-09-05 14:27:36 +0800

    add

---
+++ app/src/main/java/com/example/zjubme/teethmanagement/CuteActivity.java
@@ -0,0 +1,33 @@
+package com.example.zjubme.teethmanagement;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class CuteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cute);
+        Button button1 = (Button) findViewById(R.id.exitApp);
+        Button button2 = (Button) findViewById(R.id.CuteAc_to_LogAc);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                android.os.Process.killProcess(android.os.Process.myPid());
+                System.exit(0);
+            }
+        });
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(CuteActivity.this, LogActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
--- app/src/main/java/com/example/zjubme/teethmanagement/LogActivity.java
+++ app/src/main/java/com/example/zjubme/teethmanagement/LogActivity.java
@@ -1,13 +1,33 @@
 package com.example.zjubme.teethmanagement;
 
+import android.content.Context;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class LogActivity extends AppCompatActivity {
+    private int countSeconds = 60;
+    private EditText mobile_login,yan_zheng_ma;
+    private Button getCaptcha, loginButton;
+    private Context myComtext;
+    private String usersuccess;
+    private Handler myCountHandler = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log);
+        TextView getCaptcha = (TextView) findViewById(R.id.getCaptcha);
+        getCaptcha.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
